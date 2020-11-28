174c44ef05b55af1dda9669817c1a07e5db37efc
withlet11 <withlet11@gmail.com>
2020-11-28 13:03:59 +0100

    Changed the shape and the color of clock hands.

--- app/src/main/java/io/github/withlet11/skyclock/view/ClockHandsPanel.kt
+++ app/src/main/java/io/github/withlet11/skyclock/view/ClockHandsPanel.kt
@@ -22,8 +22,7 @@
 package io.github.withlet11.skyclock.view
 
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Paint
+import android.graphics.*
 import android.util.AttributeSet
 import io.github.withlet11.skyclock.R
 import java.time.LocalTime
@@ -44,9 +43,109 @@ class ClockHandsPanel(context: Context?, attrs: AttributeSet?) : AbstractPanel(c
         }
 
     private val paint = Paint().apply { isAntiAlias = true }
-    private val hourHandsColor = context?.getColor(R.color.transparentBlue2) ?: 0
-    private val minuteHandsColor = context?.getColor(R.color.transparentBlue3) ?: 0
-    private val secondHandsColor = context?.getColor(R.color.transparentBlue1) ?: 0
+    private val path = Path()
+    private val hourHandsColor = context?.getColor(R.color.transparentBlue3) ?: 0
+    private val minuteHandsColor = context?.getColor(R.color.transparentBlue1) ?: 0
+    private val secondHandsColor = context?.getColor(R.color.transparentWhite) ?: 0
+    private val shadow = context?.getColor(R.color.smoke) ?: 0
+
+    private val hourHandGeometries = listOf(
+        0.0f to -20.0f,
+        -5.2f to -19.3f,
+        -10.0f to -17.3f,
+        -14.1f to -14.1f,
+        -17.3f to -10.0f,
+        -19.3f to -5.2f,
+        -20.0f to 0.0f,
+        -19.3f to 5.2f,
+        -17.3f to 10.0f,
+        -14.1f to 14.1f,
+        -10.0f to 17.3f,
+        -10.0f to 236.0f,
+        -9.5f to 238.0f,
+        -8.0f to 239.5f,
+        -6.0f to 240.0f,
+        6.0f to 240.0f,
+        8.0f to 239.5f,
+        9.5f to 238.0f,
+        10.0f to 236.0f,
+        10.0f to 17.3f,
+        14.1f to 14.1f,
+        17.3f to 10.0f,
+        19.3f to 5.2f,
+        20.0f to 0.0f,
+        19.3f to -5.2f,
+        17.3f to -10.0f,
+        14.1f to -14.1f,
+        10.0f to -17.3f,
+        5.2f to -19.3f
+    )
+
+    private val minuteHandGeometries = listOf(
+        0.0f to -16.0f,
+        -4.1f to -15.5f,
+        -8.0f to -13.9f,
+        -11.3f to -11.3f,
+        -13.9f to -8.0f,
+        -15.5f to -4.1f,
+        -16.0f to 0.0f,
+        -15.5f to 4.1f,
+        -13.9f to 8.0f,
+        -11.3f to 11.3f,
+        -8.0f to 13.9f,
+        -8.0f to 380.0f,
+        -7.5f to 382.0f,
+        -6.0f to 383.5f,
+        -4.0f to 384.0f,
+        4.0f to 384.0f,
+        6.0f to 383.5f,
+        7.5f to 382.0f,
+        8.0f to 380.0f,
+        8.0f to 13.9f,
+        11.3f to 11.3f,
+        13.9f to 8.0f,
+        15.5f to 4.1f,
+        16.0f to 0.0f,
+        15.5f to -4.1f,
+        13.9f to -8.0f,
+        11.3f to -11.3f,
+        8.0f to -13.9f,
+        4.1f to -15.5f
+    )
+
+    private val secondHandGeometries = listOf(
+        0.0f to -12.0f,
+        -3.1f to -11.6f,
+        -6.0f to -10.4f,
+        -8.5f to -8.5f,
+        -10.4f to -6.0f,
+        -11.6f to -3.1f,
+        -12.0f to 0.0f,
+        -11.6f to 3.1f,
+        -10.4f to 6.0f,
+        -8.5f to 8.5f,
+        -6.0f to 10.4f,
+        -3.1f to 11.6f,
+        -2.0f to 382.5f,
+        -1.8f to 383.3f,
+        -1.3f to 383.8f,
+        -0.5f to 384.0f,
+        0.5f to 384.0f,
+        1.3f to 383.8f,
+        1.8f to 383.3f,
+        2.0f to 382.5f,
+        3.1f to 11.6f,
+        6.0f to 10.4f,
+        8.5f to 8.5f,
+        10.4f to 6.0f,
+        11.6f to 3.1f,
+        12.0f to 0.0f,
+        11.6f to -3.1f,
+        10.4f to -6.0f,
+        8.5f to -8.5f,
+        6.0f to -10.4f,
+        3.1f to -11.6f
+    )
 
     @Suppress("RedundantOverride")
     override fun performClick(): Boolean {
@@ -66,28 +165,73 @@ class ClockHandsPanel(context: Context?, attrs: AttributeSet?) : AbstractPanel(c
 
     private fun Canvas.drawHourHand() {
         save()
+        translate(5f, 5f)
         rotate(180f / 6f * (localTime.toSecondOfDay() / 3600f + 6f))
+        paint.maskFilter = BlurMaskFilter(2f, BlurMaskFilter.Blur.NORMAL)
+        paint.color = shadow
+        paint.style = Paint.Style.FILL
+        hourHandGeometries.last().let { (x, y) -> path.moveTo(x, y) }
+        hourHandGeometries.forEach { (x, y) -> path.lineTo(x, y) }
+        drawPath(path, paint)
+        path.reset()
+        restore()
+        save()
+        rotate(180f / 6f * (localTime.toSecondOfDay() / 3600f + 6f))
+        paint.maskFilter = null
         paint.color = hourHandsColor
         paint.style = Paint.Style.FILL
-        drawRect(-5f, -40f, 5f, 240f, paint)
+        hourHandGeometries.last().let { (x, y) -> path.moveTo(x, y) }
+        hourHandGeometries.forEach { (x, y) -> path.lineTo(x, y) }
+        drawPath(path, paint)
+        path.reset()
         restore()
     }
 
     private fun Canvas.drawMinuteHand() {
         save()
+        translate(5f, 5f)
         rotate(180f / 30f * (localTime.minute + localTime.second / 60f + 30f))
+        paint.maskFilter = BlurMaskFilter(2f, BlurMaskFilter.Blur.NORMAL)
+        paint.color = shadow
+        paint.style = Paint.Style.FILL
+        minuteHandGeometries.last().let { (x, y) -> path.moveTo(x, y) }
+        minuteHandGeometries.forEach { (x, y) -> path.lineTo(x, y) }
+        drawPath(path, paint)
+        path.reset()
+        restore()
+        save()
+        rotate(180f / 30f * (localTime.minute + localTime.second / 60f + 30f))
+        paint.maskFilter = null
         paint.color = minuteHandsColor
         paint.style = Paint.Style.FILL
-        drawRect(-4f, -40f, 4f, 384f, paint)
+        minuteHandGeometries.last().let { (x, y) -> path.moveTo(x, y) }
+        minuteHandGeometries.forEach { (x, y) -> path.lineTo(x, y) }
+        drawPath(path, paint)
+        path.reset()
         restore()
     }
 
     private fun Canvas.drawSecondHand() {
         save()
+        translate(5f, 5f)
         rotate(180f / 30f * (localTime.second + 30f))
+        paint.maskFilter = BlurMaskFilter(2f, BlurMaskFilter.Blur.NORMAL)
+        paint.color = shadow
+        paint.style = Paint.Style.FILL
+        secondHandGeometries.last().let { (x, y) -> path.moveTo(x, y) }
+        secondHandGeometries.forEach { (x, y) -> path.lineTo(x, y) }
+        drawPath(path, paint)
+        path.reset()
+        restore()
+        save()
+        rotate(180f / 30f * (localTime.second + 30f))
+        paint.maskFilter = null
         paint.color = secondHandsColor
         paint.style = Paint.Style.FILL
-        drawRect(-1.5f, -40f, 1.5f, 384f, paint)
+        secondHandGeometries.last().let { (x, y) -> path.moveTo(x, y) }
+        secondHandGeometries.forEach { (x, y) -> path.lineTo(x, y) }
+        drawPath(path, paint)
+        path.reset()
         restore()
     }
 
