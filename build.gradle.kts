1336cd898307c78fbc17063208a02d8476f21ad7
withlet11 <withlet11@gmail.com>
2026-07-23 21:17:55 +0200

    Updated Kotlin, KSP, and Gradle versions.
    - Updated Kotlin to 2.4.10 in `build.gradle.kts` and the version catalog.
    - Updated KSP plugin version to 2.3.10.
    - Updated Gradle Wrapper to 9.6.1.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
-    val kotlin_version by extra("2.2.10")
+    val kotlin_version by extra("2.4.10")
     repositories {
         google()
         mavenCentral()
@@ -16,7 +16,7 @@ buildscript {
 }
 
 plugins {
-    id("com.google.devtools.ksp") version "2.3.2" apply false
+    id("com.google.devtools.ksp") version "2.3.10" apply false
 }
 
 tasks.register("clean", Delete::class) {
